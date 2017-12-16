2c7c0131431f53ac303d698f2daf3e326f2d8a27
Rebecca <rvandyke27@gmail.com>
2017-12-16 14:05:33 -0500

    Added empty functions
    I'm sorry I'm not being productive

---
+++ MACLayerSimulation.m
@@ -0,0 +1,8 @@
+function [ output_args ] = MACLayerSimulation( input_args )
+%MACLayerSimulation simulation of MAC Layer
+%   describe input parameters
+
+
+
+end
+
---
+++ physicalLayerSimulation.m
@@ -0,0 +1,9 @@
+function [  ] = physicalLayerSimulation( input_args )
+%physicalLayerSimulation runs simulation of physical layer protocols
+%   description of input parameters
+
+
+
+
+end
+
