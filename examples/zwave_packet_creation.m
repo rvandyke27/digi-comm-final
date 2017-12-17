57a455f844e42ddc3167bd4f39aec80befbfb9b5
Avi Webberman <aviwebberman@Avis-MacBook-Pro.local>
2017-12-16 19:08:21 -0500

    added message and some stuff

--- examples/zwave_packet_creation.m
+++ examples/zwave_packet_creation.m
@@ -1,4 +1,13 @@
-% 1. create n bit (or byte?) message (bit stream)
+% 1. create n bit (or byte?) message (bit stream) could be actual packet or
+% random
+msgLen = 120;                     % length in bits
+message = randi([0 1], msgLen, 1);  % transmitted message
+M = 2;        % Modulation order
+k = log2(M);
+freqsep = 40000;  % Frequency separation (Hz)
+nsamp = 2000;    % Number of samples per symbol
+Fs = 80000;      % Sample rate (Hz)
+zwaveform = fskmod(message, M,freqsep,nsamp,Fs);
 
 % 2. create modulated signal based on nrz coding 
  %print out chunk of frequency modulated message (waveform)
@@ -13,5 +22,18 @@
 % display chunk of waveform
 % 6. demodulate 
 % show demodulated chunk?
-
+  
+  %find average snr for error free transmission?
+  received = awgn(zwaveform, 0);
+  bits     = fskdemod(received, M, freqsep, nsamp, Fs);
+  
+  if(message == bits)
+     disp("no errors"); 
+ 
+  
+  else
+    disp("an error");
+    
+  end
+  
 % 7. full show receieved frame
